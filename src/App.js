c1f4becb19fe85fff868611ada702d01bcfb0648
Nick Chiong <kulastv@gmail.com>
2021-06-11 00:19:32 +0800

    Adding Header, Cart and Meals components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
